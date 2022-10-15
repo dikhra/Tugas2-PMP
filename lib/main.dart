ed55f39b97ed99419d4dda602b3ff86405d384a4
Dimas Khalik Rahmahdin <dimaskhalik77@gmail.com>
2022-10-15 09:32:18 +0700

    Revisi Code 1
    menaikan code karena salah posisi

--- lib/main.dart
+++ lib/main.dart
@@ -51,60 +51,64 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const CircleAvatar(
-          backgroundImage: AssetImage('images/pic.jpg'),
-          radius: 50.0,
-        ),
-        Text(
-          'Dimas Khalik Rahmahdin',
-          style: GoogleFonts.pacifico(
-              fontSize: 35.0, color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        const Text(
-          'Coding & Learng a new thing!',
-          style: TextStyle(
-              fontSize: 20.0, letterSpacing: 1.5, color: Colors.white),
-        )
-      ],
-    ));
-    Container(
-      padding: const EdgeInsets.all(10.0),
-      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-      color: Colors.white,
-      child: Row(
-        children: const [
-          Padding(
-              padding: EdgeInsets.only(right: 10.0, left: 10.0),
-              child: Icon(
-                Icons.phone_android,
-                color: Colors.teal,
-              )),
-          Text(
-            '085886149964',
-            style: TextStyle(color: Colors.teal, fontSize: 20.0),
-          )
-        ],
-      ),
-    );
-    Container(
-      padding: const EdgeInsets.all(10.0),
-      margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 25.0),
-      color: Colors.white,
-      child: Row(
-        children: const [
-          Padding(
-            padding: EdgeInsets.only(right: 10.0, left: 10.0),
-            child: Icon(
-              Icons.email,
-              color: Colors.teal,
-            ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircleAvatar(
+            backgroundImage: AssetImage('images/pic.jpg'),
+            radius: 50.0,
           ),
           Text(
-            'dimaskhalik77@gmail.com',
-            style: TextStyle(color: Colors.teal, fontSize: 20.0),
+            'Dimas Khalik Rahmahdin',
+            style: GoogleFonts.pacifico(
+                fontSize: 35.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+          ),
+          const Text(
+            'Coding & Learng a new thing!',
+            style: TextStyle(
+                fontSize: 20.0, letterSpacing: 1.5, color: Colors.white),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            margin:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            color: Colors.white,
+            child: Row(
+              children: const [
+                Padding(
+                    padding: EdgeInsets.only(right: 10.0, left: 10.0),
+                    child: Icon(
+                      Icons.phone_android,
+                      color: Colors.teal,
+                    )),
+                Text(
+                  '085886149964',
+                  style: TextStyle(color: Colors.teal, fontSize: 20.0),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 25.0),
+            color: Colors.white,
+            child: Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(right: 10.0, left: 10.0),
+                  child: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                ),
+                Text(
+                  'dimaskhalik77@gmail.com',
+                  style: TextStyle(color: Colors.teal, fontSize: 20.0),
+                )
+              ],
+            ),
           )
         ],
       ),
